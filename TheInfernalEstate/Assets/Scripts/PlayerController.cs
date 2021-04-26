d626d32792e8a41aefc3f457706be930cda8e61c
harry-04 <72386526+harry-04@users.noreply.github.com>
2021-04-26 16:12:39 +0100

    InventoryPart1

---
+++ TheInfernalEstate/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Interactable focus;
+
+    Camera cam;
+    
+    void Start()
+    {
+        cam = Camera.main;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("We hit " + hit.collider.name + " " + hit.point);
+
+                RemoveFocus();
+            }
+        }
+       
+
+       if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+
+            // If the ray hits
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+
+                //if he did set it as our focus
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                }
+            }
+        }
+    }
+
+    void SetFocus(Interactable newFocus)
+    {
+        if (newFocus != focus)
+        {
+            if (focus != null)
+                focus.OnDefocused();
+            
+
+
+            focus = newFocus;
+        }
+
+        
+        newFocus.OnFocused(transform);
+    }
+
+    void RemoveFocus()
+    {
+        if (focus != null)
+            focus.OnDefocused();
+
+        
+        focus = null;
+    }
+}
